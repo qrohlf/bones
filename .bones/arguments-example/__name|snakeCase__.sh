b18c3327f6d0b3092f9fd7bead122be88a997af9
Quinn Rohlf <qr@qrohlf.com>
2019-07-25 10:23:17 -0600

    support __var|helper__ syntax

---
+++ .bones/arguments-example/__name|snakeCase__.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# you can pass simple arguments to bones templates.
+#
+# run `bones arguments-example myExample log="foo bar baz" to try it out`
+echo "you setup this template with the name __name__"
+echo "and passed it '__log__' for the log argument"
+echo "here it is camelCased: __log|camelCase__"
+echo "here it is PascalCased: __log|pascalCase__"
+echo "here it is snake_cased: __log|snakeCase__"
+echo "here it is kebab-cased: __log|kebabCase__"
